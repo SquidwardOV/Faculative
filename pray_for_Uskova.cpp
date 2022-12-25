31b0b211284fd1e9ff351f7db3e8f1a5de96eb35
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2022-12-26 00:19:45 +0300

    Create pray_for_Uskova.cpp

---
+++ pray_for_Uskova.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+int main()
+{
+    srand(time(0));
+    int a[4][5], * p;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            a[i][j] = rand() % 10;
+        }
+    }
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 5; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "\n\n\n\n\n\n";
+    for (int i = 0; i < 4; ++i) {
+        p = a[i];
+        for (int j = 0; j < 5; j++) {
+            *(p + j) = 5 * i + j + 1;
+            cout << a[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+    getchar();
+    return 0;
+}
