cfa161675f830bf1c560e11376ec39ab301a572c
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2023-03-11 20:37:13 +0300

    Update laba2forLiza.cpp

--- laba2forLiza.cpp
+++ laba2forLiza.cpp
@@ -2,52 +2,66 @@
 #include <fstream>
 #include <string>
 
+using namespace std;
+
+const int N = 3; // Количество сотрудников
+
 struct Employee {
-    std::string surname;
-    std::string name;
-    std::string patronymic;
-    std::string department;
-    std::string position;
-    int birth_year;
-    float monthly_salary[12];
-    char gender;
+    string lastName;
+    string firstName;
+    string middleName;
+    string department;
+    string position;
+    int birthYear;
+    int salary[12];
+    string gender;
 };
 
 int main() {
-    std::ifstream input("employees.txt");
-    int N;
-    std::string department_name;
-    input >> N >> department_name;
-
     Employee employees[N];
-    for (int i = 0; i < N; i++) {
-        input >> employees[i].surname >> employees[i].name >> employees[i].patronymic
-              >> employees[i].department >> employees[i].position >> employees[i].birth_year
-              >> employees[i].monthly_salary[0] >> employees[i].monthly_salary[1]
-              >> employees[i].monthly_salary[2] >> employees[i].monthly_salary[3]
-              >> employees[i].monthly_salary[4] >> employees[i].monthly_salary[5]
-              >> employees[i].monthly_salary[6] >> employees[i].monthly_salary[7]
-              >> employees[i].monthly_salary[8] >> employees[i].monthly_salary[9]
-              >> employees[i].monthly_salary[10] >> employees[i].monthly_salary[11]
-              >> employees[i].gender;
-    }
 
-    for (int i = 0; i < N; i++) {
-        int age = 2023 - employees[i].birth_year;
-        if (age < 60 && employees[i].department == department_name) {
-            employees[i].monthly_salary[0] *= 1.15;
+    // Считываем данные о сотрудниках из файла
+    ifstream in_file("employees.txt");
+    if (!in_file) {
+        cerr << "Cannot open input file" << endl;
+        return 1;
+    }
+    for (int i = 0; i < N; ++i) {
+        in_file >> employees[i].lastName >> employees[i].firstName >> employees[i].middleName;
+        in_file >> employees[i].department >> employees[i].position >> employees[i].birthYear;
+        for (int j = 0; j < 12; ++j) {
+            in_file >> employees[i].salary[j];
+        }
+        in_file >> employees[i].gender;
+    }
+    in_file.close();
+
+    // Изменяем зарплату сотрудников
+    string department_name;
+    cout << "Enter department name: ";
+    cin >> department_name;
+
+    for (int i = 0; i < N; ++i) {
+        if (employees[i].department == department_name && 2023 - employees[i].birthYear < 60) {
+            employees[i].salary[0] *= 1.15; // Увеличиваем зарплату за январь на 15%
         }
     }
 
-    for (int i = 0; i < N; i++) {
-        std::cout << employees[i].surname << " " << employees[i].name << " " << employees[i].patronymic
-                  << " " << employees[i].department << " " << employees[i].position << " "
-                  << employees[i].birth_year << " ";
-        for (int j = 0; j < 12; j++) {
-            std::cout << employees[i].monthly_salary[j] << " ";
-        }
-        std::cout << employees[i].gender << std::endl;
+    // Выводим обновленные данные о сотрудниках в файл
+    ofstream out_file("updated_employees.txt");
+    if (!out_file) {
+        cerr << "Cannot open output file" << endl;
+        return 1;
     }
+    for (int i = 0; i < N; ++i) {
+        out_file << employees[i].lastName << " " << employees[i].firstName << " " << employees[i].middleName << endl;
+        out_file << employees[i].department << " " << employees[i].position << " " << employees[i].birthYear << endl;
+        for (int j = 0; j < 12; ++j) {
+            out_file << employees[i].salary[j] << " ";
+        }
+        out_file << employees[i].gender << endl;
+    }
+    out_file.close();
 
     return 0;
 }
