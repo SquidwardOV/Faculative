37f1fd0365e147fc805cf9eb5efe3f4f3214d168
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2023-03-23 21:02:40 +0300

    Update asp.cpp

--- asp.cpp
+++ asp.cpp
@@ -2,18 +2,6 @@
 
 using namespace std;
 
-void row_sort(double * arr, int n) {
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = i + 1; j < n; j++) {
-      if ( * (arr + i) < * (arr + j)) {
-        double temp = * (arr + i);
-        *(arr + i) = * (arr + j);
-        *(arr + j) = temp;
-      }
-    }
-  }
-}
-
 double row_min(double * arr, int n) {
   double min = * arr;
   for (int i = 1; i < n; i++) {
@@ -24,12 +12,28 @@ double row_min(double * arr, int n) {
   return min;
 }
 
-double row_product(double * arr, int n) {
-  double product = 1;
-  for (int i = 0; i < n; i++) {
-    product *= * (arr + i);
+double col_max(double ** arr, int n, int col) {
+  double max = arr[0][col];
+  for (int i = 1; i < n; i++) {
+    if (arr[i][col] > max) {
+      max = arr[i][col];
+    }
   }
-  return product;
+  return max;
+}
+
+double ** matrix_multiply(double ** A, double ** B, int n) {
+  double ** C = new double * [n];
+  for (int i = 0; i < n; i++) {
+    C[i] = new double[n];
+    for (int j = 0; j < n; j++) {
+      C[i][j] = 0;
+      for (int k = 0; k < n; k++) {
+        C[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+  return C;
 }
 
 int main() {
@@ -49,69 +53,49 @@ int main() {
     }
   }
 
-  // Find row with max sum
-  int max_row = 0;
-  double max_sum = 0;
-  for (int i = 0; i < n; i++) {
-    double row_sum = 0;
-    for (int j = 0; j < n; j++) {
-      row_sum += A[i][j];
-    }
-    if (row_sum > max_sum) {
-      max_sum = row_sum;
-      max_row = i;
-    }
-  }
-
-  // Sort rows of matrix A in descending order of values in max row
-  double max_row_values[n];
-  for (int i = 0; i < n; i++) {
-    max_row_values[i] = A[max_row][i];
-  }
-  row_sort(max_row_values, n);
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      B[i][j] = A[i][j];
-    }
-  }
-
-  for (int i = 0; i < n; i++) {
-    double current_row_values[n];
-    for (int j = 0; j < n; j++) {
-      current_row_values[j] = A[i][j];
-    }
-    row_sort(current_row_values, n);
-
-    for (int j = 0; j < n; j++) {
-      if (current_row_values[j] == max_row_values[0]) {
-        for (int k = 0; k < n; k++) {
-          B[i][k] = A[max_row][k];
-        }
-        break;
-      }
-    }
-  }
-
-  // Output matrix B
-  cout« "Matrix B:"«
+  // Input matrix B
+  cout« "Enter the elements of matrix B:"«
   endl;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
-      cout« B[i][j]«
+      cin» B[i][j];
+    }
+  }
+
+  // Create array of row minima for matrix A
+  double A_row_minima[n];
+  for (int i = 0; i < n; i++) {
+    double row_values[n];
+    for (int j = 0; j < n; j++) {
+      row_values[j] = A[i][j];
+    }
+    A_row_minima[i] = row_min(row_values, n);
+  }
+
+  // Create array of column maxima for matrix B
+  double B_col_maxima[n];
+  for (int i = 0; i < n; i++) {
+    B_col_maxima[i] = col_max(B, n, i);
+  }
+
+  // Multiply row minima of A by column maxima of B
+  double C[n][n];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      C[i][j] = A_row_minima[i] * B_col_maxima[j];
+    }
+  }
+
+  // Output matrix C
+  cout« "Matrix C:"«
+  endl;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cout« C[i][j]«
       " ";
     }
     cout« endl;
   }
-
-  // Find min element and product of values in max row
-  double min_element = row_min(max_row_values, n);
-  double product = row_product(max_row_values, n);
-
-  cout« "Min element in max row: "«
-  min_element« endl;
-  cout« "Product of values in max row: "«
-  product« endl;
 
   return 0;
 }
